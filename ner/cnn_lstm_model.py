40873da07dd6f537c551e408d58a76ce670511c6
Majid <mjlaali@gmail.com>
2016-09-16 14:45:59 -0400

    Save models and evalutate

--- ner/cnn_lstm_model.py
+++ ner/cnn_lstm_model.py
@@ -1,15 +1,18 @@
+import pickle
+
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.layers import Bidirectional, TimeDistributed
-from keras.models import Sequential, load_model, model_from_json
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.core import Dense, Dropout, Merge, Reshape, Activation
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-import pickle
-from keras.optimizers import RMSprop, Adam
+from keras.models import Sequential, load_model
+from keras.optimizers import Adam
 from keras.regularizers import l2
+import numpy as np
 
 from ner.preprocess_nn import NnPreprocessor
+from ner.report import bio_classification_report
 
 
 class CNN_LSTM_Model:
@@ -77,24 +80,31 @@ def test_model(model_file):
         preprocessor = pickle.load(input)
 
     test_file = os.path.join(os.path.dirname(__file__), '../data/test.txt')
-    test_sents, test_tags = preprocessor.read_tagged_file(test_file)
+    test_sents, y_test = preprocessor.read_tagged_file(test_file)
     test_words_vecs, test_chars_vecs = preprocessor.convert_word_to_index(test_sents)
-    test_tags_vec = preprocessor.convert_tags_to_index(test_tags)
+    test_tags_vec = preprocessor.convert_tags_to_index(y_test)
     X_word_test, X_char_test = preprocessor.pad_dataset(test_words_vecs, test_chars_vecs)
     X_char_test = X_char_test.reshape(X_char_test.shape[0], X_char_test.shape[1] * X_char_test.shape[2])
     Y_test = preprocessor.pad_tags(test_tags_vec)
+    _, tag_dict, _, _ = preprocessor.get_dictionaries_and_max()
+    reversed_tag_dict = dict(zip(tag_dict.values(), tag_dict.keys()))
 
     model = load_model(model_file)
-    scores = model.evaluate([X_char_test, X_word_test], Y_test)
+    Y_predict = model.predict([X_char_test, X_word_test])
+    Y_predict = np.argmax(Y_predict, axis=2)
 
-    for i in range(len(model.metrics_names)):
-        print('{}: {:.4f}'.format(model.metrics_names[i], scores[i]))
+    y_pred = list()
+    for i in range(Y_predict.shape[0]):
+        y_list = Y_predict[i].tolist()
+        y_pred.append([reversed_tag_dict[y] for y in y_list if y > 0])
+
+    print(bio_classification_report(y_test, y_pred))
 
 def test():
-    model_file = os.path.join(os.path.dirname(__file__), '../data/models/best_model.20.hdf5')
+    model_file = os.path.join(os.path.dirname(__file__), '../data/models/best_model.39.hdf5')
     test_model(model_file)
 
-def main():
+def train_model():
     train_file = os.path.join(os.path.dirname(__file__), '../data/train.txt')
     wordvects_file = os.path.join(os.path.dirname(__file__), '../data/wordvecs.txt')
     preprocessor = NnPreprocessor(train_file, wordvects_file)
@@ -117,6 +127,6 @@ def main():
 
 import time
 if __name__ == "__main__":
-    main()
+    #train_model()
     test()
-    time.sleep(1) # delays for 5 seconds
+    time.sleep(1) # delays to close the tensorflow session
---
+++ ner/report.py
@@ -0,0 +1,29 @@
+from itertools import chain
+
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelBinarizer
+#from http://nbviewer.jupyter.org/github/tpeng/python-crfsuite/blob/master/examples/CoNLL%202002.ipynb
+
+def bio_classification_report(y_true, y_pred):
+    """
+    Classification report for a list of BIO-encoded sequences.
+    It computes token-level metrics and discards "O" labels.
+
+    Note that it requires scikit-learn 0.15+ (or a version from github master)
+    to calculate averages properly!
+    """
+    lb = LabelBinarizer()
+    y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
+    y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+
+    tagset = set(lb.classes_) - {'O'}
+    tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+    class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+
+    return classification_report(
+        y_true_combined,
+        y_pred_combined,
+        labels=[class_indices[cls] for cls in tagset],
+        target_names=tagset,
+        digits=5
+    )
