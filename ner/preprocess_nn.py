2d8872e66748ebd91a930a7f62363ea59e0e6ef9
Majid <mjlaali@gmail.com>
2016-09-17 19:36:44 -0400

    remove unnecessary methods

--- ner/preprocess_nn.py
+++ ner/preprocess_nn.py
@@ -80,64 +80,9 @@ class NnPreprocessor:
         self.max_sent_len = max(sents_len)
         self.max_word_len = max(words_len)
 
-
-    def convert_tags_to_index(self, sent_tags):
-        # Convert tags
-        Y_train = list()
-        for a_tag_seq in sent_tags:
-            y_train = []
-            for tag in a_tag_seq:
-                y_train.append(self.tag_dictionary[tag])
-            Y_train.append(y_train)
-        return Y_train
-
-
     def get_train_dataset(self):
         return self.sent_words, self.sent_tags
 
-
-    def pad_dataset(self, raw_word_train, raw_char_train):
-        X_word_padded = pad_sequences(raw_word_train, self.max_sent_len)
-        X_char = []
-        for i in range(len(raw_char_train)):
-            padded_sequence = pad_sequences([[] for k in range(self.max_sent_len - len(raw_char_train[i]))],
-                                            maxlen=self.max_word_len).tolist() + \
-                              pad_sequences(raw_char_train[i], maxlen=self.max_word_len).tolist()
-            X_char.append(padded_sequence)
-
-        X_char_padded = np.array(X_char)
-        return X_word_padded, X_char_padded
-
-    def pad_tags(self, raw_tag_train):
-        Y_padded_train = pad_sequences(raw_tag_train, self.max_sent_len)
-        Y_one_hot = np.zeros((len(Y_padded_train), self.max_sent_len, self.nb_classes))
-        for i in range(len(raw_tag_train)):
-            Y_one_hot[i] = np_utils.to_categorical(Y_padded_train[i], nb_classes=self.nb_classes)
-        return Y_one_hot
-
-    def get_padded_train_datatset(self):
-        try:
-            return self.X_word_padded, self.X_char_padded, self.Y_one_hot
-        except:
-            raw_word_train, raw_char_train, raw_tag_train = self.get_train_dataset()
-            self.X_word_padded, self.X_char_padded = self.pad_dataset(raw_word_train, raw_char_train)
-            self.Y_one_hot = self.pad_tags(raw_tag_train)
-
-            return self.X_word_padded, self.X_char_padded, self.Y_one_hot
-
-    def get_raw(selfs):
-        return selfs.sent_words, selfs.sent_tags
-
-    def get_dictionaries_and_max(self):
-        return self.word_dictionary, self.tag_dictionary, self.max_sent_len, self.max_word_len
-
-    def get_embedding_weights(self):
-        vocab_dim = 301  # dimensionality of your word vectors
-        n_symbols = len(self.word_dictionary) + 1  # adding 1 to account for 0th index (for masking)
-        embedding_weights = np.zeros((n_symbols, vocab_dim))
-        # for word, index in self.word_dictionary.items():
-        #     embedding_weights[index, :] = self.word_embedding[word]
-        return embedding_weights
 
 def main():
     train_dataset_file = '../data/coling2016/train'
@@ -149,7 +94,7 @@ def main():
     X_chars = pad_sequences_2D(sent_words_chars, max_sent_len, max_word_len)
 
     reader = PretrainedWordVectorReader(word_vector_file)
-    reader.load(set(itertools.chain(*sent_words)), delimiter=' ')
+    reader.load(set(itertools.chain(*sent_words)), delimiter='\t')
     sent_words_int = reader.convert_dataset(sent_words)
     X_words = pad_sequences_2D(sent_words_int, max_sent_len, reader.get_len_embedding_vector(), dtype='float32')
 
